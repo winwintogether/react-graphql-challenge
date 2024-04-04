b3aa5be970a81407298b3f95b7057bd7ac148986
winwintogether <masterinbusiness@outlook.com>
2024-04-04 16:06:57 +0200

    chore: install chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import Login from './Pages/Login';
+import Account from './Pages/Account';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Router>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/account" component={Account} />
+          <Route path="/" exact component={Login} />
+        </Switch>
+      </Router>
+    </ChakraProvider>
   );
-}
+};
 
 export default App;
