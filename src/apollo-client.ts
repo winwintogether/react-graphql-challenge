5838c0e1e377577d1d1ff363570ccb7686104190
winwintogether <masterinbusiness@outlook.com>
2024-04-04 15:00:59 +0200

    chore: add authorization in graphql

--- src/apollo-client.ts
+++ src/apollo-client.ts
@@ -1,11 +1,26 @@
-import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink, ApolloLink } from '@apollo/client';
 
 const httpLink = new HttpLink({
   uri: 'https://cms.trial-task.k8s.ext.fcse.io/graphql',
 });
 
+const authLink = new ApolloLink((operation, forward) => {
+  // Retrieve the authentication token from local storage
+  const token = localStorage.getItem('authToken');
+
+  // Use the setContext method to set the HTTP headers.
+  operation.setContext({
+    headers: {
+      Authorization: token ? `Bearer ${token}` : '',
+    }
+  });
+
+  // Call the next link in the middleware chain.
+  return forward(operation);
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink), // Chain it with the http link
   cache: new InMemoryCache(),
 });
 
